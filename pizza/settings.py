54573e7e2d0e4baf68dfc8106241dbd873b9866a
DYSIM <simdayang@gmail.com>
2020-06-04 19:32:35 +0800

    added STATICFILES_DIRS for static files

--- pizza/settings.py
+++ pizza/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#
+#static file directory inclusion
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR,
+'static',
+),
+)

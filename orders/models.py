5d9665a8cd18fc06217d28bfa544503b6c211658
DYSIM <simdayang@gmail.com>
2020-06-04 19:34:59 +0800

    added pizza model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class pizza(models.Model):
+    type =  models.CharField(max_length=20) #Pizza or Sub or Pasta  or Salad or Dinner Platter?
+    upsize = models.BooleanField() #Big or Small
+    pizzaDetails = models.CharField(max_length=10) #Cheese, 1,2,or 3 toppings, special
+    price = models.DecimalField(max_digits=4, decimal_places=2)

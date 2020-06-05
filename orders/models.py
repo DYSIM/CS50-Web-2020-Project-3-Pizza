86562cacb0504da70f2845a03a293014093355af
DYSIM <simdayang@gmail.com>
2020-06-05 22:46:21 +0800

    added models for other items in menu

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class pizza(models.Model):
-    type =  models.CharField(max_length=20) #Pizza or Sub or Pasta  or Salad or Dinner Platter?
+    type =  models.CharField(max_length=30) #Pizza or Sub or Pasta  or Salad or Dinner Platter?
     upsize = models.BooleanField() #Big or Small
     pizzaDetails = models.CharField(max_length=10) #Cheese, 1,2,or 3 toppings, special
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+class sub(models.Model):
+    type = models.CharField(max_length=30)
+    upsize = models.BooleanField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+class pasta(models.Model):
+    type = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+class salads(models.Model):
+    type = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+class dinnerPlatters(models.Model):
+    type = models.CharField(max_length=30)
+    upsize = models.BooleanField()
     price = models.DecimalField(max_digits=4, decimal_places=2)

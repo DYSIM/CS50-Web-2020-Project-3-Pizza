5a118b677e5c9724566655b4207eea21bb265a9b
DYSIM <simdayang@gmail.com>
2020-06-08 01:17:53 +0800

    pass pizza details from database to html upon login to populate menu

--- orders/views.py
+++ orders/views.py
@@ -2,13 +2,21 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-
+from orders.models  import pizza
+import math
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return render(request,"login.html",{"message":None})
+    print(pizza.objects.all().values())
+    pizza_entries = list(pizza.objects.all()) #force list to split half
+    half = int(math.ceil(len(pizza_entries)/2))
+    pizza1 = pizza_entries[:half]
+    pizza2 = pizza_entries[half:]
     context = {
-        "user" : request.user
+        "user" : request.user,
+        "pizza1":  pizza1,
+        "pizza2":  pizza2
     }
     return render(request, 'user.html',context)
 

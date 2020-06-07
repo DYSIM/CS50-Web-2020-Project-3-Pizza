49b373e1b21972dd8627803cef73464f5945e15f
DYSIM <simdayang@gmail.com>
2020-06-08 01:14:16 +0800

    made changes to database model and added entries (upsize boolean to size charfield)

--- orders/models.py
+++ orders/models.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import User
 
 class pizza(models.Model):
     type =  models.CharField(max_length=30)
-    upsize = models.BooleanField() #Big or Small
+    size = models.CharField(max_length=5) #Big or Small
     pizzaDetails = models.CharField(max_length=10) #Cheese, 1,2,or 3 toppings, special
     price = models.DecimalField(max_digits=4, decimal_places=2)
 
 class sub(models.Model):
     type = models.CharField(max_length=30)
-    upsize = models.BooleanField()
+    size = models.CharField(max_length=5)
     price = models.DecimalField(max_digits=4, decimal_places=2)
 
 class pasta(models.Model):
@@ -24,9 +24,8 @@ class salads(models.Model):
 
 class dinnerPlatters(models.Model):
     type = models.CharField(max_length=30)
-    upsize = models.BooleanField()
+    size = models.CharField(max_length=5)
     price = models.DecimalField(max_digits=4, decimal_places=2)
 
 class toppings(models.Model):
     type = models.CharField(max_length=30)
-    

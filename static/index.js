b7dd64711036cdad8c0831de416d27cb9f6f8ecd
DYSIM <simdayang@gmail.com>
2020-06-09 23:56:16 +0800

    not really needed now

--- static/index.js
+++ static/index.js
@@ -6,17 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
   for (var i=0; i<picture.length; i++){
     var x = picture[i]
     x.onclick = function(click) {
-      var body = document.getElementsByTagName("body")[0]
-      var foodMenu = document.getElementById('Menu')
-      var dataset = x.dataset;
-      var divElement = document.createElement('div');
-      divElement.setAttribute('class','jumbotron jumbotron-fluid')
-      for (d in dataset) {
-        divElement.innerHTML += dataset[d];
+      if (document.contains(document.getElementById("orderPlaceHolder"))) {
+        document.getElementById("orderPlaceHolder").remove()
       }
-      // divElement.innerHTML = 'hello'
+      var orders = document.getElementById('userOrders')
+      var dataset = x.dataset;
+      var pElement = document.createElement('p');
+      for (d in dataset) {
+        let content =
+          `${d}: ${dataset[d]}\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0`;
+        pElement.innerHTML += content;
 
-      body.insertBefore(divElement,foodMenu)
+        
+      }
+
+      orders.appendChild(pElement)
       click.preventDefault();
     }
   }

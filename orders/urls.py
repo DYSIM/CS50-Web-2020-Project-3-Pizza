558fb5c76e61fe07d72ab121c5dcd04db4bcf12e
DYSIM <simdayang@gmail.com>
2020-06-10 21:59:05 +0800

    added cart page and url to submit and delete order

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view,name="login"),
     path('logout',views.logout_view, name='logout'),
-    path('submitorderpizza',views.submitorderpizza,name="submitorderpizza")
+    path('submitorderpizza',views.submitorderpizza,name="submitorderpizza"),
+    path('submitordersub',views.submitordersub,name="submitordersub"),
+    path('cart',views.cart_view,name="cart"),
+    path('delete',views.delete_from_cart,name='delete')
 ]

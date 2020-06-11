8369686bb8fdfd71e57f7d114b0b574aacbf87e8
DYSIM <simdayang@gmail.com>
2020-06-12 02:35:56 +0800

    script to change cart and trackorder elements dynamically (delete and complete)

--- static/index.js
+++ static/index.js
@@ -19,33 +19,34 @@ function getCookie(cname) {
 document.addEventListener('DOMContentLoaded', () => {
 
 
+
 // remove row and delete from database on clicking trashbin on cart page
   var trash = document.getElementsByClassName("cart-trash")
   for(var i = 0; i < trash.length; i++){
     trash[i].onclick = function() {
       var box = this
-      var price = box.parentNode.previousSibling.firstChild.innerHTML;
+      var price = box.parentNode.previousSibling.previousSibling.firstChild.innerHTML;
 
       var order_id = box.dataset.id
-      var xhttp = new XMLHttpRequest();
-      xhttp.open("POST", '/delete')
-      xhttp.onload = () => {
+      var trashRequest = new XMLHttpRequest();
+      trashRequest.open("POST", '/delete')
+      trashRequest.onload = () => {
         this.parentNode.parentNode.style.background = 'yellow';
         setTimeout(function(){
             box.parentNode.parentNode.style.display = 'none';
         },500);
 
-        totalPriceElement = document.getElementById('priceTotal');
+        var totalPriceElement = document.getElementById('priceTotal');
         currTotalPrice = totalPriceElement.innerHTML;
         totalPriceElement.innerHTML = (parseFloat(currTotalPrice) - parseFloat(price)).toFixed(2);
 
 
       }
      var csrftoken = getCookie('csrftoken');
-     xhttp.setRequestHeader("X-CSRFToken", csrftoken);
+     trashRequest.setRequestHeader("X-CSRFToken", csrftoken);
      const data_ = new FormData();
      data_.append('order_id', order_id);
-     xhttp.send(data_);
+     trashRequest.send(data_);
      return false;
 
 
@@ -53,8 +54,42 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 // remove row and delete from database on clicking trashbin on cart page
 
+// submit cart orders
+  var submitCartButton = document.getElementById('submitCart');
+  submitCartButton.onclick = () => {
+    var submitCartRequest = new XMLHttpRequest();
+    var csrftoken = getCookie('csrftoken');
 
+    submitCartRequest.open("POST", '/submitcart');
+    submitCartRequest.setRequestHeader("X-CSRFToken", csrftoken);
+    submitCartRequest.onload = () => {
+      var cartEntries = document.getElementsByClassName("cartEntries")
+      var cartModalBody = document.getElementById('cartModalBody');
+      var cartModalRedirect = document.getElementById('cartModalRedirect');
+      var count = 0
 
+      for(var i = 0; i < cartEntries.length; i++){
+        if (cartEntries[i].style.display !== 'none'){
+          count+=1;
+          cartEntries[i].style.display='none';
+        }
+      }
+      if (count=== 0){
+        cartModalBody.innerHTML = 'You have no orders!'
+        cartModalRedirect.innerHTML = 'Go to menu'
+        cartModalRedirect.href = '/';
+      }
+      else{
+        cartModalBody.innerHTML = 'Your orders have been submitted to the kitchen! Thank you for ordering.'
+        cartModalRedirect.innerHTML = 'Track your orders'
+        cartModalRedirect.href = 'trackorders';
+      }
+      var totalPriceElement = document.getElementById('priceTotal');
+      totalPriceElement.innerHTML = "0.00"
+    }
+    submitCartRequest.send()
+  }
+// submit cart orders
 
 
 })
---
+++ static/trackorders.js
@@ -0,0 +1,97 @@
+
+//getCookie function
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+// remove row and delete from database on clicking trashbin on cart page
+  var trash = document.getElementsByClassName("trackorder-trash-button")
+  for(var i = 0; i < trash.length; i++){
+    trash[i].onclick = function() {
+      var box = this
+      var price = box.parentNode.previousSibling.previousSibling.firstChild.innerHTML;
+
+      var order_id = box.dataset.id
+      var trashRequest = new XMLHttpRequest();
+      trashRequest.open("POST", '/delete')
+      trashRequest.onload = () => {
+        this.parentNode.parentNode.style.background = 'yellow';
+        setTimeout(function(){
+            box.parentNode.parentNode.style.display = 'none';
+        },500);
+
+        if (box.parentNode.parentNode.parentNode.id === 'completedOrdersBody'){
+          var totalPriceElement = document.getElementById('completedPriceTotal');
+        }
+        else{
+          var totalPriceElement = document.getElementById('pendingPriceTotal');
+        }
+
+        currTotalPrice = totalPriceElement.innerHTML;
+        totalPriceElement.innerHTML = (parseFloat(currTotalPrice) - parseFloat(price)).toFixed(2);
+
+
+      }
+     var csrftoken = getCookie('csrftoken');
+     trashRequest.setRequestHeader("X-CSRFToken", csrftoken);
+     const data_ = new FormData();
+     data_.append('order_id', order_id);
+     trashRequest.send(data_);
+     return false;
+
+
+    }
+  }
+// remove row and delete from database on clicking trashbin on cart page
+
+
+var completed_buttons = document.getElementsByClassName("completed-button");
+for (var i = 0; i < completed_buttons.length; i++){
+  completed_buttons[i].onclick = function() {
+    var box = this;
+    var price = box.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.firstChild.innerHTML;
+    var completed_button_row = box.parentNode
+    var table_row = box.parentNode.parentNode
+    var order_id = box.dataset.id
+    var request = new XMLHttpRequest();
+    request.open("POST", '/completeorder')
+    request.onload = () => {
+      table_row.style.background= 'yellow'
+      setTimeout(function(){
+          completed_button_row.remove() //remove the delete button
+          document.getElementById('completedOrdersBody').insertBefore(table_row,document.getElementById('completedOrdersBody').lastElementChild) //shift from pending to completed table
+          table_row.style.background= 'transparent';
+      },500);
+
+      var pendingPriceElement = document.getElementById('pendingPriceTotal');
+      pendingTotalPrice = pendingPriceElement.innerHTML;
+      pendingPriceElement.innerHTML = (parseFloat(pendingTotalPrice) - parseFloat(price)).toFixed(2);
+      var completedPriceElement = document.getElementById('completedPriceTotal');
+      completedTotalPrice = completedPriceElement.innerHTML;
+      completedPriceElement.innerHTML = (parseFloat(completedTotalPrice) + parseFloat(price)).toFixed(2);
+    }
+    var csrftoken = getCookie('csrftoken');
+    request.setRequestHeader("X-CSRFToken", csrftoken);
+    const data_ = new FormData();
+    data_.append('order_id', order_id);
+    request.send(data_);
+    return false;
+  }
+}
+})
